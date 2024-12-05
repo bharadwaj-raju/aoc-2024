1b4cd6c8cdac4f9a9b3bb9f6f3e10d368b656b3b
Bharadwaj Raju <bharadwaj.raju777@gmail.com>
2024-12-05 14:51:36 +0530

    day 5 edit: change to toposort instead of swapping elements

--- day5/2.py
+++ day5/2.py
@@ -1,15 +1,33 @@
 from util import readlines
 
 
-def topologically_valid(
-    E: set[tuple[int, int]], ordering: list[int]
-) -> tuple[bool, tuple[int, int] | None]:
+def outgoing(E: set[tuple[int, int]], source: int) -> set[int]:
+    return {dst for src, dst in E if src == source}
+
+
+def incoming(E: set[tuple[int, int]], destination: int) -> set[int]:
+    return {src for src, dst in E if dst == destination}
+
+
+# Kahn's algorithm
+def toposort(V: set[int], E: set[tuple[int, int]]) -> list[int]:
+    order = []
+    independent = {v for v in V if not incoming(E, v)}
+    while independent:
+        n = independent.pop()
+        order.append(n)
+        for m in outgoing(E, n):
+            E.remove((n, m))
+            if not incoming(E, m):
+                independent.add(m)
+
+    return order
+
+
+def topologically_valid(E: set[tuple[int, int]], ordering: list[int]) -> bool:
     indices = {n: i for i, n in enumerate(ordering)}
     subE = {(u, v) for u, v in E if u in indices and v in indices}
-    for u, v in subE:
-        if not indices[u] < indices[v]:
-            return (False, (indices[u], indices[v]))
-    return (True, None)
+    return all(indices[u] < indices[v] for u, v in subE)
 
 
 E: set[tuple[int, int]] = set()
@@ -23,18 +41,14 @@ for line in readlines():
     else:
         orderings.append([int(x) for x in line.split(",")])
 
+
 midsummer = 0
 
 for ordering in orderings:
-    if not topologically_valid(E, ordering)[0]:
-        valid = False
-        while not valid:
-            valid, wrong = topologically_valid(E, ordering)
-            if valid:
-                break
-            assert wrong is not None
-            u, v = wrong
-            ordering[u], ordering[v] = ordering[v], ordering[u]
+    if not topologically_valid(E, ordering):
+        V = set(ordering)
+        subE = {(u, v) for u, v in E if u in V and v in V}
+        ordering = toposort(V, subE)
         mid = ordering[len(ordering) // 2]
         midsummer += mid
 
