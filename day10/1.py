cfc312d49f87bb34b10398a501514df8ed453092
Bharadwaj Raju <bharadwaj.raju777@gmail.com>
2024-12-10 11:06:57 +0530

    day 10

---
+++ day10/1.py
@@ -0,0 +1,26 @@
+from functools import reduce
+from util import vec2, readgrid, grid_get
+
+grid = readgrid()
+
+
+def score(grid: list[list[str]], start: vec2) -> set[vec2]:
+    curr = grid_get(grid, start, "-10")
+    if curr == "8":
+        return set(nb for nb in start.cardinal_neighbors() if grid_get(grid, nb) == "9")
+    return reduce(
+        set.union,
+        (score(grid, nb) for nb in start.cardinal_neighbors() if int(grid_get(grid, nb, "-10")) == int(curr) + 1),
+        set(),
+    )
+
+
+scoresum = 0
+
+for y, row in enumerate(grid):
+    for x, cell in enumerate(row):
+        if cell == "0":
+            sc = score(grid, vec2(x, y))
+            scoresum += len(sc)
+
+print(scoresum)
---
+++ day10/2.py
@@ -0,0 +1,24 @@
+from functools import reduce
+from util import vec2, readgrid, grid_get
+
+grid = readgrid()
+
+
+def rating(grid: list[list[str]], start: vec2) -> int:
+    curr = grid_get(grid, start, "-10")
+    if curr == "8":
+        return sum(grid_get(grid, nb) == "9" for nb in start.cardinal_neighbors())
+    return sum(
+        rating(grid, nb) for nb in start.cardinal_neighbors() if int(grid_get(grid, nb, "-10")) == int(curr) + 1
+    )
+
+
+ratingsum = 0
+
+for y, row in enumerate(grid):
+    for x, cell in enumerate(row):
+        if cell == "0":
+            ra = rating(grid, vec2(x, y))
+            ratingsum += ra
+
+print(ratingsum)
